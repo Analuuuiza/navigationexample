ceedc05bfbfbef5027b7b3bfb9deded0c527adf5
analuuuiza <anaedara123@gmail.com>
2024-05-02 15:08:52 -0300

    exemplo

---
+++ src/screens/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Head from "../components/Head";
+import Footer from "../components/Footer";
+
+
+function Home(): React.JSX.Element {
+    return (
+       <View>
+        <Head />
+        <Text>Tela Principal</Text>
+        <Footer />
+       </View>
+    );
+}
+
+export default Home;
